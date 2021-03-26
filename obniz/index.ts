93be3847693184f2a8bf6b1abbd167e78316ead8
iemong <kiyoshidainagon@gmail.com>
2021-03-27 00:53:51 +0900

    analog read普通にできてたわ

--- obniz/index.ts
+++ obniz/index.ts
@@ -1,10 +1,27 @@
-import Obniz from "obniz";
+import * as dotenv from "dotenv";
+import Obniz from 'obniz';
+dotenv.config()
 
-const obniz = new Obniz("");
+const obniz = new Obniz(process.env.OBNIZ_ID || '')
+
+// obniz.onconnect = async () => {
+//     obniz.io2?.output(false)
+//     obniz.io3?.drive('5v')
+//     obniz.io3?.output(true)
+//     obniz.io0?.pull('5v')
+//     obniz.io0?.input(bool => {
+//         if(bool) {
+//             console.log('on')
+//         } else {
+//             console.log('off')
+//         }
+//     })
+// };
 obniz.onconnect = async () => {
     obniz.io2?.output(false)
+    obniz.io3?.drive('5v')
     obniz.io3?.output(true)
     obniz.ad0?.start(num => {
-        console.log(`changed to ${num}v`)
+        console.log(num)
     })
 };

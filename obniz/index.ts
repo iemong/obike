b261a01e666d521e57763bb6a1b25a3939450396
iemong <kiyoshidainagon@gmail.com>
2021-03-27 22:27:02 +0900

    ピン配置を変更

--- obniz/index.ts
+++ obniz/index.ts
@@ -4,24 +4,13 @@ dotenv.config()
 
 const obniz = new Obniz(process.env.OBNIZ_ID || '')
 
-// obniz.onconnect = async () => {
-//     obniz.io2?.output(false)
-//     obniz.io3?.drive('5v')
-//     obniz.io3?.output(true)
-//     obniz.io0?.pull('5v')
-//     obniz.io0?.input(bool => {
-//         if(bool) {
-//             console.log('on')
-//         } else {
-//             console.log('off')
-//         }
-//     })
-// };
 obniz.onconnect = async () => {
-    obniz.io2?.output(false)
     obniz.io3?.drive('5v')
     obniz.io3?.output(true)
+    obniz.io2?.output(false)
     obniz.ad0?.start(num => {
-        console.log(num)
+        if(num < 2) {
+            console.log(num)
+        }
     })
 };

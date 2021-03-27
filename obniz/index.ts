7bdb6ba3189486b0ff0792b51855d132ff6c50ae
iemong <kiyoshidainagon@gmail.com>
2021-03-27 23:26:53 +0900

    なんかio3はバグってるみたいなので使用を諦める。3秒おきにセンサーにヒットした平均回数を吐き出すようにする

--- obniz/index.ts
+++ obniz/index.ts
@@ -4,13 +4,27 @@ dotenv.config()
 
 const obniz = new Obniz(process.env.OBNIZ_ID || '')
 
+let on = false
+let counter = 0
+
 obniz.onconnect = async () => {
-    obniz.io3?.drive('5v')
-    obniz.io3?.output(true)
+    obniz.io4?.drive('5v')
+    obniz.io4?.output(true)
     obniz.io2?.output(false)
+    obniz.io1?.output(false)
     obniz.ad0?.start(num => {
-        if(num < 2) {
-            console.log(num)
+        if(num > 0.01) {
+            if(!on) {
+                counter++
+                on = true
+            }
+        } else {
+            on = false
         }
     })
 };
+
+setInterval(() => {
+    console.log(counter / 3)
+    counter = 0
+}, 3000)

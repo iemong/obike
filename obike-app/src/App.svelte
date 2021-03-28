ac0870591d0dc77718487afd1a80270e5e6bfcfe
iemong <kiyoshidainagon@gmail.com>
2021-03-29 00:29:40 +0900

    youtubeの動画を再生できるところまで作る

--- obike-app/src/App.svelte
+++ obike-app/src/App.svelte
@@ -1,31 +1,55 @@
-<main>
-    <h1>Hello {name}!</h1>
-    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <script lang="ts">
-	export let name:string;
-	name = 'iemong'
+  import { onMount } from "svelte";
+
+  let player = null;
+
+  onMount(() => {
+    window.onYouTubeIframeAPIReady = () => {
+      console.log("hoge");
+      player = new YT.Player("player", {
+        videoId: "ih19o9c-xDo",
+        playerVars: {
+          loop: 1,
+          controls: 0,
+          autoplay: 1,
+          showinfo: 0,
+          rel: 0,
+        },
+        events: {
+          onReady: (event) => {
+            event.target.playVideo();
+          },
+          onStateChange: (event) => {
+            console.log(event)
+          },
+        },
+      });
+    };
+  });
 </script>
 
+<main>
+  <div id="player"></div>
+</main>
+
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
